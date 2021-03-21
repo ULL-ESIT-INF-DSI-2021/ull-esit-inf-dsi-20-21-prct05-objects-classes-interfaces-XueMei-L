e96705e3c5603b848071f35d6a92dd0e0d8bf1e0
XueMei-L <72436762+XueMei-L@users.noreply.github.com>
2021-03-22 00:52:22 +0100

    Delete src directory

--- src/ejercicio-1/ejercicio-1.ts
+++
@@ -1,8 +0,0 @@
-import {Pokemon} from './Pokemon';
-import {Combat} from './Combat';
-
-let pokemon_p1 = new Pokemon("Charizard", 0.6, 8.5, "Fuego", [52, 43, 65, 282],);
-let pokemon_p2 = new Pokemon("Squirtle", 0.5, 9, "Agua", [48, 65, 43, 292]);
-
-let battle :Combat = new Combat(pokemon_p1, pokemon_p2);
-battle.start();
